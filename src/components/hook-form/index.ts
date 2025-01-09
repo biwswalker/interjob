18f9f4550ea7583a7e3f025ad40608256109bb2d
Biws <biws@Biws-Mac-mini.local>
2025-01-10 00:51:45 +0700

    add ESLint configuration; remove unused RHFCodes component

--- src/components/hook-form/index.ts
+++ src/components/hook-form/index.ts
@@ -3,7 +3,6 @@ export * from './RHFCheckbox'
 
 export { default } from './FormProvider'
 
-export { default as RHFCodes } from './RHFCodes'
 export { default as RHFSwitch } from './RHFSwitch'
 export { default as RHFSelect } from './RHFSelect'
 export { default as RHFSlider } from './RHFSlider'
